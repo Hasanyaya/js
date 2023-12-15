449d50238d45b21d7564e50a20c174f1c7cd55ee
Mohd Hasan <153216664+Hasanyaya@users.noreply.github.com>
2023-12-15 10:52:12 +0000

    Change loop

---
+++ Lecture-04/loop.js
@@ -0,0 +1,67 @@
+ /* 1. for( let i = 1; i<=10; i++)              ....For loop
+{
+    console.log(i);
+}*/
+
+2. // print all odd num from (1 to 15)
+/*let i =1;
+for(i=1; i<=15; i =i+2){
+    console.log(i);
+}*/
+
+             3. // print all even number(2 to 10)
+/*let i =2;
+for(i=2; i<=20; i= i+2){
+    console.log(i);
+}*/
+
+             6.// print tha multiplication for 5
+/*let i =5;
+for(i= 5; i<=50; i=i+5){
+    console.log(i);
+}*/
+
+                               //......nested loop
+/*for(let i=1;i<=3;i++){
+    for(let j=1;j<=3;j++){
+        console.log(j);
+    }
+}*/
+
+
+                               //...While loop
+/*let i =1;
+while (i<=10) {
+    console.log(i);
+    i++;
+    
+}   */
+
+                            //....Break Keyword
+
+  /*let i =1;
+while (i<=10) {
+    if (i==3){
+        break;
+    }
+    console.log(i);
+    i++;                                          
+
+}*/
+
+
+                             //....loop with arrays
+
+ /* let fruits = ["mango","apple","banan","litchi","orangs"];
+  for( let i= 0; i<fruits.length;i++){
+    console.log(i,fruits[i]);
+  }     */
+
+                            //...For of loop
+ let fruits = ["mango","apple","banan","litchi","orangs"];
+ for(i of fruits){
+    console.log(i);
+ }
+
+
+ 
